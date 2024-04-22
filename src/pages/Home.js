b944bbc41fc3c6167a97cb9c7bb35a5615bf35e6
ahmar7 <ahmarjabbar7@gmail.com>
2024-04-22 22:48:19 +0500

    navbar

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,14 @@
 import React from "react";
+import Header from "../layout/Header/Header";
+import Banner from "../components/Home/Banner/Banner";
 
 const Home = () => {
-  return <div></div>;
+  return (
+    <div className="home-bg">
+      <Header />
+      <Banner />
+    </div>
+  );
 };
 
 export default Home;
